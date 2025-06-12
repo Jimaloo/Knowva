2a0498f2df14790b3c5f0586578019b3e15ca23f
Jim Juma <jimaloo43@gmail.com>
2025-06-12 19:38:32 +0300

    so it begins...

---
+++ composeApp/src/commonTest/kotlin/com/knowva/app/ComposeAppCommonTest.kt
@@ -0,0 +1,12 @@
+package com.knowva.app
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class ComposeAppCommonTest {
+
+    @Test
+    fun example() {
+        assertEquals(3, 1 + 2)
+    }
+}
